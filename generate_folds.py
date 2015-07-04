27bb229ea1a7f364867e8ce24c7ddee552cafa24
Jason Feriante <jason.feriante@gmail.com>
2015-07-03 23:23:48 -0500

    cleaning the data progress

--- generate_folds.py
+++ generate_folds.py
@@ -70,61 +70,142 @@ def get_target(fname, data_type):
 
 
 
-def make_folds(filenames, activity, data_type):
+def parse_line(line, activity, data_type):
+    if(data_type == 'DUD-E' or data_type == 'MUV'):
+
+        parts = line.rstrip('\n').split(r' ')
+        native_id = parts[0]
+        bitstring = parts[1]
+        # generate sha1 hash identity for bitstring
+        sha_1.update(bitstring)
+        hash_id = sha_1.hexdigest()
+
+        # activity is based on the fname / argument passed in
+        if(activity == '_actives'):
+            activity = 1
+        else:
+            activity = 0
+
+        # row format:
+        # hash_id, native_id, activity, bitstring, fold
+        row = [hash_id, native_id, activity, bitstring, 0]
+
+        return row
+
+    elif(data_type == 'Tox21'):
+
+        parts = line.rstrip('\n').split(r' ')
+        activity = parts[0]
+        native_id = parts[1]
+        bitstring = parts[2]
+        # generate sha1 hash identity for bitstring
+        sha_1.update(bitstring)
+        hash_id = sha_1.hexdigest()
+
+        # row format:
+        # hash_id, native_id, activity, bitstring, fold
+        row = [hash_id, native_id, activity, bitstring, 0]
+
+        return row
+
+    elif(data_type == 'PCBA'):
+        raise ValueError('PCBA method not written yet')
+    else:
+        raise ValueError('Unknown data type:' + str(data_type))
+
+
+
+def write_folds(filename, all_rows, is_active):
+    """ each file only gets 2x folds: actives & inactive """
+    num_rows = len(all_rows)
+    fold_size = num_rows / 5
+    last_fold_size = (num_rows % 5) + fold_size
+
+    # sanity check for the fold math
+    assert((last_fold_size + fold_size * 4) == num_rows)
+
+    count = 0
+    for fold_id in range(5):
+        for row_id in range(fold_size):
+            with open(filename, 'w') as file_obj:
+                print all_rows[row_id]
+                print all_rows.pop()
+                exit(0)
+                # row = all_rows.pop()
+                # row[4] = is_active
+                # print row
+                # print activity
+                # exit(0)
+                # row = ' '.join(str(v) for v in all_rows.pop())
+                # print 'row'
+                # print row
+                # exit(0)
+                # file_obj.write(row + '\n')
+
+    print 'wtf'
+    exit(0)
+    # confirm we built all the folds
+    # assert(len(all_rows) == count)
+
+
+
+def make_folds(filenames, activity, data_type, data_path, fold_path, csv_path = None):
     """ activity (string): actives or inactives """
     """ data_type (string): DUD-E, MUV, Tox21, PCBA """
     # parse the target names from the files
     for fname in filenames:
+
         target = get_target(fname, data_type)
 
         # filenames
         row = []
-        all_inactives = []
+        all_rows = []
         with open(data_path + '/' + fname) as f:
             lines = f.readlines()
             for line in lines:
-                parts = line.rstrip('\n').split(r' ')
-                native_id = parts[0]
-                bitstring = parts[1]
-                # generate sha1 hash identity for bitstring
-                sha_1.update(bitstring)
-                hash_id = sha_1.hexdigest()
-
-                # row format:
-                # hash_id, native_id, bitstring
-                row = [hash_id, native_id, 0, bitstring]
-                all_inactives.append(row)
+                row = parse_line(line, activity, data_type)
+                all_rows.append(row)
 
         # generate folder for our folds:
         mkdir_p(fold_path)
 
         # randomize
-        random.shuffle(all_inactives)
-        num_rows = len(all_inactives)
-        fold_size = num_rows / 5
-        last_fold_size = (num_rows % 5) + fold_size
+        random.shuffle(all_rows)
 
-        # sanity check for the fold math
-        assert((last_fold_size + fold_size * 4) == num_rows)
+        # write our files
+        if(data_type == 'Tox21'):
 
-        # generate folds 1-4 for this dataset
-        for i in range(4):
-            # base the file name on the current fold
-            filename = fold_path + '/' + target + '_' + activity +'_fold' + str(i) + '.txt'
-            with open(filename, 'w') as file_obj:
-                for j in range(fold_size):
-                        row = ' '.join(str(v) for v in all_inactives.pop())
-                        file_obj.write(row + '\n')
+            # build our active & inactive files respectively
+            actives = []
+            inactives = []
+            for row in all_rows:
+                is_active = row[4]
+                if(is_active):
+                    actives.append(row)
+                else:
+                    inactives.append(row)
 
-        # generate the 5th fold (which might be an odd number)
-        filename = fold_path + '/' + target + '_' + activity +'_fold4.txt'
-        with open(filename, 'w') as file_obj:
-            for j in range(last_fold_size):
-                row = ' '.join(str(v) for v in all_inactives.pop())
-                file_obj.write(row + '\n')
+            # release the rows from memory
+            del all_rows
 
-        assert ( len(all_inactives) == 0)
+            #write actives
+            filename = target + '_actives.fl'
+            write_folds(filename, actives, 1)
 
+            #write inactives
+            filename = target + '_inactives.fl'
+            write_folds(filename, inactives, 0)
+
+        else:
+
+            # It's not Tox21
+
+            is_active = 0
+            if(activity == '_actives'):
+                is_active = 1
+
+            filename = target + activity + '.fl'
+            write_folds(filename, all_rows, is_active)
 
 
 def dud_e():
@@ -137,12 +218,16 @@ def dud_e():
 
     for dir_name, sub, files in os.walk(data_path):
         for fname in files:
-            if 'inactives' in fname:
-                # print('inactive: %s' % fname)
-                inactives.append(fname)
+            if fname.startswith('.'):
+                # ignore system files
+                pass
             else:
-                # print('  active: %s' % fname)
-                actives.append(fname)
+                if 'inactives' in fname:
+                    # print('inactive: %s' % fname)
+                    inactives.append(fname)
+                else:
+                    # print('  active: %s' % fname)
+                    actives.append(fname)
 
     # we should have an equal number of files
     assert len(actives) == len(inactives)
@@ -150,8 +235,8 @@ def dud_e():
     print "Now generating folds for DUD-E..."
 
     # pass along the file names
-    make_folds(actives, 'actives', 'DUD-E')
-    make_folds(inactives, 'inactives', 'DUD-E')
+    make_folds(actives, '_actives', 'DUD-E', data_path, fold_path)
+    make_folds(inactives, '_inactives', 'DUD-E', data_path, fold_path)
 
 
 
@@ -165,12 +250,16 @@ def muv():
 
     for dir_name, sub, files in os.walk(data_path):
         for fname in files:
-            if 'decoys' in fname:
-                # print('inactive: %s' % fname)
-                inactives.append(fname)
+            if fname.startswith('.'):
+                # ignore system files
+                pass
             else:
-                # print('  active: %s' % fname)
-                actives.append(fname)
+                if 'decoys' in fname:
+                    # print('inactive: %s' % fname)
+                    inactives.append(fname)
+                else:
+                    # print('  active: %s' % fname)
+                    actives.append(fname)
 
     # we should have an equal number of files
     assert len(actives) == len(inactives)
@@ -178,32 +267,31 @@ def muv():
     print "Now generating folds for MUV..."
 
     # pass along the file names
-    make_folds(actives, 'actives', 'MUV')
-    make_folds(inactives, 'inactives', 'MUV')
+    make_folds(actives, '_actives', 'MUV', data_path, fold_path)
+    make_folds(inactives, '_inactives', 'MUV', data_path, fold_path)
+
 
 
 # Tox21 has classifications inline (unlike the rest of the system)
 def tox21():
     """ Generate folds for Tox21 """
-    actives = []
-    inactives = []
+    all_files = []
 
     data_path = data_paths[2]
     fold_path = fold_paths[2]
 
     for dir_name, sub, files in os.walk(data_path):
         for fname in files:
-            if '.log' in fname:
+            if '.log' in fname or fname.startswith('.'):
                 pass
             else:
-                inactives.append(fname)
+                all_files.append(fname)
 
     # we only have 1 group of files!
-    print "Tox21: " + str(len(inactives)) + " files found"
+    print "Tox21: " + str(len(all_files)) + " files found"
     print "Now generating folds for Tox21..."
 
-    # XXXXXXXXXXXXXXXXXXXXXXXXXXX This needs to be modified to work correctly XXXXXXXXXXXXXXXXXXXXXXX
-    make_folds(inactives, 'inactives', 'Tox21')
+    make_folds(all_files, '', 'Tox21', data_path, fold_path)
 
 
 
@@ -235,14 +323,16 @@ def pcba():
     print "Now generating folds for MUV..."
 
     # pass along the file names
-    make_folds(actives, 'actives', 'MUV')
-    make_folds(inactives, 'inactives', 'MUV')
+    make_folds(actives, 'actives', 'MUV', data_path, csv_path)
+    make_folds(inactives, 'inactives', 'MUV', data_path, csv_path)
+
+
 
 def main(args):
     # generate DUDE-E folds
-    dud_e()
+    # dud_e()
     # muv()
-    # tox21()
+    tox21()
     # pcba()
 
 
@@ -251,5 +341,3 @@ if __name__ == "__main__":
     main(sys.argv)
 
 
-
-file_obj.write("Look, ma, I'm writing to a new file!")
