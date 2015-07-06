4c0edab65e67d2bb9d94add126680515d7a742fd
Jason Feriante <jason.feriante@gmail.com>
2015-07-05 23:24:41 -0500

    k-fold only oversamples in memory right before running a target

--- logistic_regression.py
+++ logistic_regression.py
@@ -77,47 +77,33 @@ def build_targets(fold_path, data_type):
 
 
 
-def get_folds(data_type):
-    fold_path = get_fold_path(data_type)
-    targets = build_targets(fold_path, data_type)
-
-    print "Found " + str(len(targets)) + " targets for " + data_type
-
+def get_folds(data_type, fold_path, target, fnames):
     # store folds by target
-    all_folds = {}
-    for target, fnames in targets.iteritems():
-        # generate a fold set fo reach target
-        folds = {}
-        for i in range(5):
-            # don't forget -- we are using strings & not integer keys!!!
-            folds[i] = []
+    folds = {}
+    for i in range(5):
+        # don't forget -- we are using strings & not integer keys!!!
+        folds[i] = []
 
-        #fnames contains all files for this target
-        for fname in fnames:
-            row = []
-            with open(fold_path + '/' + fname) as f:
-                lines = f.readlines()
-                for line in lines:
-                    # put each row in it's respective fold
-                    fold, row = parse_line(line, data_type)
-                    folds[int(fold)].append(row)
-
-        # add the folds to our target
-        # print 'target: ' + target
-        # print len(folds)
-        all_folds[target] =  folds
+    #fnames contains all files for this target
+    for fname in fnames:
+        row = []
+        with open(fold_path + '/' + fname) as f:
+            lines = f.readlines()
+            for line in lines:
+                # put each row in it's respective fold
+                fold, row = parse_line(line, data_type)
+                folds[int(fold)].append(row)
 
     """ Debug """
     # print "length of all folds"
     # print len(folds)
     # print "length of respective folds"
-    # print len(folds['0'])
-    # print len(folds['1'])
-    # print len(folds['2'])
-    # print len(folds['3'])
-    # print len(folds['4'])
-
-    return all_folds
+    # print len(folds[0])
+    # print len(folds[1])
+    # print len(folds[2])
+    # print len(folds[3])
+    # print len(folds[4])
+    return folds
 
 
 
@@ -157,6 +143,7 @@ def logistic_regression(target, X, Y, X_test, Y_test):
         str(num_correct / float(total))
 
 
+
 def oversample(data):
     # balance the number of actives / inactives in the dataset
     actives = []
@@ -181,26 +168,30 @@ def oversample(data):
     # print len(oversamples)
     # print total_inactives
     # print len(oversamples + inactives)
-    
+
     # combine oversampled actives + inactives into one list
     return oversamples + inactives
 
 
 
-def tox21():
-    # get our 5x folds...
-    all_folds = get_folds('Tox21')
+def run_predictions(data_type):
 
-    for target, folds in all_folds.iteritems():
-        # lets try a generic dataset with just 1 fold
+    fold_path = get_fold_path(data_type)
+    targets = build_targets(fold_path, data_type)
+    print "Found " + str(len(targets)) + " targets for " + data_type
+
+    for target, fnames in targets.iteritems():
+
+        print 'Building data for target: ' + target
+        folds = get_folds(data_type, fold_path, target, fnames)
+
         temp_data = []
         for i in range(len(folds) - 1):
             temp_data += folds[i]
 
-        test_data = folds[4]
+        test_data = folds[len(folds) - 1]
         
-        print 'Building data for target: ' + target
-
+        # print 'Oversampling the datasets to balance actives / inactives'
         # oversample our datasets
         temp_data = oversample(temp_data)
         test_data = oversample(test_data)
@@ -242,9 +233,22 @@ def tox21():
 
         logistic_regression(target, X, Y, X_test, Y_test)
 
-        # all done with 1 target....
-        exit(0)
 
+
+def tox21():
+    run_predictions('Tox21')
+
+
+def dud_e():
+    run_predictions('DUD-E')
+
+
+def muv():
+    run_predictions('MUV')
+
+
+def pcba():
+    run_predictions('PCBA')
 
 
 def main(args):
