94df843bcd766a4a91cc386786842ff620f8eb68
Jason Feriante <jason.feriante@gmail.com>
2015-07-06 12:48:30 -0500

    refactored logistic_regression to run all folds for Tox21 now.

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 from bitstring import BitArray
 
+
+
 fold_paths = [
     "./folds/DUD-E",
     "./folds/MUV",
@@ -40,9 +42,12 @@ def parse_line(line, data_type):
 
     # row format: [hash_id, is_active, native_id, fold, bitstring]
     parts = line.rstrip('\n').split(r' ')
-    hash_id = parts[0]
-    is_active = parts[1]
-    native_id = parts[2]
+    # hash_id = parts[0]
+
+    # cast the string to int
+    is_active = int(parts[1])
+
+    # native_id = parts[2]
     fold = parts[3]
     bitstring = parts[4]
 
@@ -77,73 +82,6 @@ def build_targets(fold_path, data_type):
 
 
 
-def get_folds(data_type, fold_path, target, fnames):
-    # store folds by target
-    folds = {}
-    for i in range(5):
-        # don't forget -- we are using strings & not integer keys!!!
-        folds[i] = []
-
-    #fnames contains all files for this target
-    for fname in fnames:
-        row = []
-        with open(fold_path + '/' + fname) as f:
-            lines = f.readlines()
-            for line in lines:
-                # put each row in it's respective fold
-                fold, row = parse_line(line, data_type)
-                folds[int(fold)].append(row)
-
-    """ Debug """
-    # print "length of all folds"
-    # print len(folds)
-    # print "length of respective folds"
-    # print len(folds[0])
-    # print len(folds[1])
-    # print len(folds[2])
-    # print len(folds[3])
-    # print len(folds[4])
-    return folds
-
-
-
-def logistic_regression(target, X, Y, X_test, Y_test):
-    print 'Running logistic regression for target: ' + target
-    logreg = linear_model.LogisticRegression(C=1e5)
-    logreg.fit(X, Y)
-
-    Z = logreg.predict(X_test)
-
-    # print X.shape
-    # print Y.shape
-    print 'Training size:'
-    print X.shape
-    print 'Test size:'
-    print X_test.shape
-    # print Y_test.shape
-
-    # print Z
-    # print Y_test
-    # Z = Z.tolist()
-    # Y_test = Y_test.tolist()
-
-    num_correct = 0
-    num_false = 0
-    for i in range(len(Z)):
-        if Z[i] == Y_test[i]:
-            num_correct += 1
-        else:
-            num_false += 1
-
-    total = len(Z)
-    print 'Total predictions: ' + str(total)
-    print 'Num correct: ' + str(num_correct)
-    print 'Num false: ' + str(num_false)
-    print 'Percent correct for ' + target + ': ' + \
-        str(num_correct / float(total))
-
-
-
 def oversample(data):
     # balance the number of actives / inactives in the dataset
     actives = []
@@ -174,81 +112,176 @@ def oversample(data):
 
 
 
+def get_folds(data_type, fold_path, target, fnames):
+    # store folds by target
+    folds = {}
+    for i in range(5):
+        # don't forget -- we are using strings & not integer keys!!!
+        folds[i] = []
+
+    #fnames contains all files for this target
+    for fname in fnames:
+        row = []
+        with open(fold_path + '/' + fname) as f:
+            lines = f.readlines()
+            for line in lines:
+                # put each row in it's respective fold
+                fold, row = parse_line(line, data_type)
+                folds[int(fold)].append(row)
+
+    """ Debug """
+    # print "length of all folds"
+    # print len(folds)
+    # print "length of respective folds"
+    # print len(folds[0])
+    # print len(folds[1])
+    # print len(folds[2])
+    # print len(folds[3])
+    # print len(folds[4])
+    
+    # oversample the folds to balance actives / inactives
+    for i in range(len(folds)):
+        folds[i] = oversample(folds[i])
+
+    return folds
+
+
+
+def logistic_regression(target, X, Y, X_test, Y_test, fold_id):
+
+    print 'Running logistic regression for target: ' + target
+    logreg = linear_model.LogisticRegression(C=1e5)
+    logreg.fit(X, Y)
+
+    Z = logreg.predict(X_test)
+
+    """ Debug """
+    # print 'Training size:'
+    # print X.shape
+    # print 'Test size:'
+    # print X_test.shape
+
+    num_correct = 0
+    num_false = 0
+    for i in range(len(Z)):
+        if Z[i] == Y_test[i]:
+            num_correct += 1
+        else:
+            num_false += 1
+
+    total = len(Z)
+
+    percent_correct = num_correct / float(total)
+
+    print 'target:' + target + ' fold:' + str(fold_id) + ' predicted: ' + \
+        str(total) + ' correct: ' + str(num_correct) + ' wrong: ' + \
+        str(num_false) + ' pct correct: ' + str(percent_correct)
+
+    return percent_correct
+
+
+
 def run_predictions(data_type):
 
     fold_path = get_fold_path(data_type)
     targets = build_targets(fold_path, data_type)
     print "Found " + str(len(targets)) + " targets for " + data_type
 
+    fold_accuracies = {}
     for target, fnames in targets.iteritems():
 
-        print 'Building data for target: ' + target
+        # retrieve our stratified folds
         folds = get_folds(data_type, fold_path, target, fnames)
 
-        temp_data = []
-        for i in range(len(folds) - 1):
-            temp_data += folds[i]
+        # shuffle the folds once upfront
+        for i in range(len(folds)):
+            random.shuffle(folds[i])
 
-        test_data = folds[len(folds) - 1]
-        
-        # print 'Oversampling the datasets to balance actives / inactives'
-        # oversample our datasets
-        temp_data = oversample(temp_data)
-        test_data = oversample(test_data)
+        pct_ct = []
+        # run 4 folds vs 1 fold with each possible scenario
+        for curr_fl in range(len(folds)):
 
-        # randomize the ordering
-        random.shuffle(temp_data)
-        random.shuffle(test_data)
+            print 'Building data for target: ' + target + ', fold: ' + str(curr_fl)
 
-        # for i in range(10):
-        #     print temp_data[i]
+            # folds 1-4
+            temp_data = []
+            for i in range(len(folds)):
+                if(i == curr_fl):
+                    # don't include the test fold
+                    continue
+                else:
+                    temp_data += folds[i]
 
-        # build training data
-        X = []
-        Y = []
-        for i in range(len(temp_data)):
-            string = BitArray(bin=str(temp_data[i][0]))
-            row = []
-            for bit in string:
-                row.append(int(bit))
-            X.append(row)
-            Y.append(int(temp_data[i][1]))
+            # vs current 5th test fold
+            test_data = folds[curr_fl]
+            
+            """ Turning 1024 bits into features is a slow process """
+            # build training data
+            X = []
+            Y = []
+            for i in range(len(temp_data)):
+                row = []
+                for bit in temp_data[i][0]:
+                    row.append(int(bit))
+                X.append(row)
+                Y.append(int(temp_data[i][1]))
 
-        X = np.array(X)
-        Y = np.array(Y)
+            X = np.array(X)
+            Y = np.array(Y)
 
-        # build test data
-        X_test = []
-        Y_test = []
-        for i in range(len(test_data)):
-            string = BitArray(bin=str(test_data[i][0]))
-            row = []
-            for bit in string:
-                row.append(int(bit))
-            X_test.append(row)
-            Y_test.append(int(test_data[i][1]))
+            # build test data
+            X_test = []
+            Y_test = []
+            for i in range(len(test_data)):
+                row = []
+                for bit in test_data[i][0]:
+                    row.append(int(bit))
+                X_test.append(row)
+                Y_test.append(int(test_data[i][1]))
 
-        X_test = np.array(X_test)
-        Y_test = np.array(Y_test)
+            X_test = np.array(X_test)
+            Y_test = np.array(Y_test)
 
-        logistic_regression(target, X, Y, X_test, Y_test)
+            pct_ct.append(logistic_regression(target, X, Y, X_test, Y_test, curr_fl))
 
+        # now get the average fold results for this target
+        accuracy = sum(pct_ct) / float(len(pct_ct))
+        print 'Results for '+ target + ':' + str(accuracy)
+        # update fold accuracies
+        fold_accuracies[target] = accuracy
+
+    print '####################  Results for ' + data_type + ' ####################'
+    # output results
+    accuracies = 0.00
+    num_targets = 0.00
+    for target, acc in fold_accuracies.iteritems():
+        print target + ' accuracy: ' + str(acc)
+        accuracies += acc
+        num_targets += 1
+
+    overall_acc = accuracies / num_targets
+    print ' overall accuracy: ' + str(overall_acc)
+    print '#######################################################'
 
 
 def tox21():
     run_predictions('Tox21')
 
 
+
 def dud_e():
     run_predictions('DUD-E')
+
 
 
 def muv():
     run_predictions('MUV')
 
 
+
 def pcba():
     run_predictions('PCBA')
+
 
 
 def main(args):
@@ -257,6 +290,7 @@ def main(args):
     # muv()
     # pcba()
     tox21()
+
 
 
 if __name__ == "__main__":
