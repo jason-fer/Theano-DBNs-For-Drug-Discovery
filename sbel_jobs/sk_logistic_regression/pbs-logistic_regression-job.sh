e532f5088a2b0c8e2ba7b7cb4485ee955a17426d
Jason Feriante <jason.feriante@gmail.com>
2015-07-10 14:24:51 -0500

    modifying jobs so they work on SBEL

--- sbel_jobs/sk_logistic_regression/pbs-logistic_regression-job.sh
+++ sbel_jobs/sk_logistic_regression/pbs-logistic_regression-job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#PBS -N Logistic_Regression
+#PBS -N MMNT-LogisticRegression
 #PBS -l nodes=1:gpus=1,walltime=01:00:00
 
 cd $PBS_O_WORKDIR
 module load cuda anaconda
-python ../../sk_logistic_regression.py
+python sk_logistic_regression.py
--- sbel_jobs/sk_random_forests/pbs-random_forests-job.sh
+++ sbel_jobs/sk_random_forests/pbs-random_forests-job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#PBS -N Random_Forests
+#PBS -N MMNT-RandomForests
 #PBS -l nodes=1:gpus=1,walltime=02:12:00
 
 cd $PBS_O_WORKDIR
 module load cuda anaconda
-python ../../sk_random_forests.py
+python sk_random_forests.py
