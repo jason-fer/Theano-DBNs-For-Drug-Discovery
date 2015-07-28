9ac94ef3f81af3e240e04bee9acf888e03787cc1
Jason Feriante <jason.feriante@gmail.com>
2015-07-28 15:35:19 -0500

    set up WID HTCondor jobs thanks to Prof Gitter's help.

---
+++ wid_jobs/th_deep_belief_net/wid-stnn_dbn-tox21.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This has to be executable
+
+echo _CONDOR_JOB_IWD $_CONDOR_JOB_IWD
+echo Cluster $cluster
+echo Process $process
+echo RunningOn $runningon
+
+# $process is your 0-indexed job index that you can use for looping
+CMD="python th_deep_belief_net.py tox21 $process"
+echo $CMD
