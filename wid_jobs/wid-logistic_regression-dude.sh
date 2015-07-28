0c51610aedf6438bbb4a2d6506c5958ec3a5e95d
Jason Feriante <jason.feriante@gmail.com>
2015-07-28 17:42:08 -0500

    trying to get condor jobs working

---
+++ wid_jobs/wid-logistic_regression-dude.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This has to be executable
+
+echo _CONDOR_JOB_IWD $_CONDOR_JOB_IWD
+echo Cluster $cluster
+echo Process $process
+echo RunningOn $runningon
+
+# $process is your 0-indexed job index that you can use for looping
+CMD="python sk_logistic_regression.py dud_e $process"
+echo $CMD
+$CMD
